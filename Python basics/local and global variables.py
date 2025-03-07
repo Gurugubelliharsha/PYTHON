82520b56d2008af8151aeef26b0ccbc702b18b1b
Gurugubelliharsha <harshagurugubelli04@gmail.com>
2025-03-07 20:19:16 +0530

    Delete Python basics directory

--- Python basics/local and global variables.py
+++
@@ -1,7 +0,0 @@
-#Define the local and Global variables with the same name and print both variables and understand the scope of the variables
-x = "Global Variable"
-def my_fun():
-    x = "Local Variable"
-    print("Inside function:", x)
-my_fun()
-print("Outside function:", x) 
