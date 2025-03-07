9e075e516410efb8f3916e18faf766e553699ec0
Gurugubelliharsha <harshagurugubelli04@gmail.com>
2025-03-07 08:11:08 +0530

    Add files via upload

---
+++ Python basics/datatypes.py
@@ -0,0 +1,12 @@
+#Define variables for different Data Types int, Boolean, char, float, double and print on the Console. 
+a = 10        # Integer
+b = True      # Boolean
+c = 'A'       # Character
+d = 10.5      # Float
+e = 20.123456789  # Double
+
+print("Integer: ", a)
+print("Boolean: ", b)
+print("Character: ", c)
+print("Float: ", d)
+print("Double: ", e)
